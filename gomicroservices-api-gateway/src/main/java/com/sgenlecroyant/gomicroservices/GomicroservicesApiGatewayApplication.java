b40833dd90a50b67a45e2e7fb7a9130029a055ea
sgenlecroyant <sgenlecroyant@gmail.com>
2022-05-23 04:27:32 +0200

    set up the API gateway app

---
+++ gomicroservices-api-gateway/src/main/java/com/sgenlecroyant/gomicroservices/GomicroservicesApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.gomicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomicroservicesApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GomicroservicesApiGatewayApplication.class, args);
+	}
+
+}
