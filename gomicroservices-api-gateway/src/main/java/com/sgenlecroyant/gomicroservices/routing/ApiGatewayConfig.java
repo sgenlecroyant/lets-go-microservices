3fc20a767bf029260025195876fa7f674c1ee135
sgenlecroyant <sgenlecroyant@gmail.com>
2022-05-26 17:18:17 +0200

    implemented API routing and loadbalanced the requests

---
+++ gomicroservices-api-gateway/src/main/java/com/sgenlecroyant/gomicroservices/routing/ApiGatewayConfig.java
@@ -0,0 +1,31 @@
+package com.sgenlecroyant.gomicroservices.routing;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+	
+	@Bean
+	public RouteLocator getRouteLocator(RouteLocatorBuilder builder) {
+		
+		return builder
+				.routes()
+				.route((p) -> p.path("/get")
+						
+						.filters(f -> {
+							return f.addResponseHeader("responseHeader", "Franck Sgen");
+							
+						})
+						
+						.uri("http://httpbin.org:80"))
+				
+				.route((p) -> p.path("/countries/**")
+						.uri("lb://country-service"))
+				
+				.build();
+	}
+
+}
